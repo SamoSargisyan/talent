c28ca76a0bd790d89c8b102cd0333792bc766aed
Samson <samvel.sargisyan@bk.ru>
2020-11-10 00:24:40 +0500

    model and cleaned welcome page

--- database/migrations/2020_11_09_190313_create_tasks_table.php
+++ database/migrations/2020_11_09_190313_create_tasks_table.php
@@ -17,7 +17,6 @@ class CreateTasksTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
-            $table->string('title');
             $table->timestamps();
         });
     }
